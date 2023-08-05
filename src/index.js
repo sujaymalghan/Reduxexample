4a2b258e5c3e4eccfb7440e0bb1725797410c602
sujay <sujayvmalghan@gmail.com>
2023-08-05 13:25:33 -0400

    Final commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import {Provider} from 'react-redux';
+import {store} from './store';
+import {createRoot} from 'react-dom/client';
+
+import 'bulma/css/bulma.css';
+import './styles.css';
+
+
+const el = document.getElementById('root');
+
+const root = createRoot(el);
+
+root.render(
+
+  <Provider store={store} >
+  
+  <App />
+
+  </Provider>
+
+
+);
+
